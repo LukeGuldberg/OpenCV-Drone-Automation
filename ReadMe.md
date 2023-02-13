2148a43e05bc4c5e04ff35fb48b66c5dcbfb2c40
Luke Guldberg <lukeguldberg@gmail.com>
2023-02-13 17:27:49 -0600

    safety

--- ReadMe.md
+++ ReadMe.md
@@ -1,8 +1,9 @@
 # Using OpenCV
-## Installing OpenCV Library
-- The task of installing OpenCV on Windows turned out to be a more challenging task than expected. The method that ended up working was downloading the library using MSYS2. MSYS2 makes downloading different softwares and libraries on Windows much easier.
-## Using Webcam
-- The very first step of using OpevCV was getting a window that displayed the webcam from my laptop. OpenCV makes this very easy, and it only requires that you set the capture to 0. 
+## <u>Week One</u>
+### Installing OpenCV Library
+- The task of installing OpenCV on Windows turned out to be a more challenging task than expected. The method that ended up working was downloading the library using MSYS2. MSYS2 makes downloading different softwares and libraries on Windows much easier. It does this by doing work behind the scenes like adding things to the file path.
+### Using Webcam
+- The very first step of using OpevCV was getting a window that displayed the webcam from my laptop. OpenCV makes this very easy, and it only requires that you set the capture to 0.   
 `cv::VideoCapture capture{0};`
 - The next step was to then convert the image captured by my webcam into grey scale. This was done easily with a helper function that converts the color of an image.
 ```c++
@@ -12,3 +13,6 @@ cv::cvtColor(frame, greyMat, cv::COLOR_BGR2GRAY);
 cv::imshow("Tello", frame);
 cv::imshow("Tello_Grey", greyMat);
 ```
+## <u>Week Two</u>
+### Picking Out Key Points
+- The idea behind this project revolves around being able to track key points....
--- webcam.cpp
+++ webcam.cpp
@@ -1,15 +1,31 @@
+#include <opencv2/core/types_c.h>
+
 #include <iomanip>
 #include <iostream>
 #include <opencv2/core/types.hpp>
 #include <opencv2/features2d.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/videoio.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/videoio.hpp>  //set
 #include <queue>
-#include <string>
+#include <set>
 #include <vector>
+namespace std {
+template <>
+struct hash<cv::Point2f> {
+    void hash_combine(std::size_t& seed, int value) const {
+        seed ^= hash<int>{}(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
 
-using namespace cv;
+    std::size_t operator()(const cv::Point2f p) const {
+        std::size_t seed = 0;
+        hash_combine(seed, p.x);
+        hash_combine(seed, p.y);
+    }
+};
+}  // namespace std
+std::set<cv::Point2f> convertToSet(std::vector<cv::Point2f> v);
 
 int main() {
     cv::VideoCapture capture{0};
@@ -18,55 +34,134 @@ int main() {
         return -1;
     }
 
+    // generaterandomcolors()
+    std::vector<cv::Scalar> colors;
+    cv::RNG rng;
+    for (int i = 0; i < 100; i++) {
+        int r = rng.uniform(0, 256);
+        int g = rng.uniform(0, 256);
+        int b = rng.uniform(0, 256);
+        colors.push_back(cv::Scalar(r, g, b));
+    }
+
     int maxCorners = 10;
     double qualityLevel = 0.01;
     double minDistance = 20.;
-    cv::Mat mask;
     int blockSize = 3;
     bool useHarrisDetector = false;
     double k = 0.04;
+    cv::Mat mask;
     std::queue<cv::Point2f> queue;
+    cv::Mat oldGray, oldFrame;  // used in calcopticalflow , last frames imgs
+    std::vector<cv::Point2f> oldCorners;  // corners for the last frame
+    std::set<cv::Point2f> points;
+    std::hash<cv::Point2f> set;
+
+    // Take first frame and find corners in it
+    capture >> oldFrame;
+    cvtColor(oldFrame, oldGray, cv::COLOR_BGR2GRAY);
+    goodFeaturesToTrack(oldGray, oldCorners, 100, 0.3, 7, cv::Mat(), 7, false,
+                        0.04);
+
+    // Create a mask image for drawing purposes
+    // Mat mask = Mat::zeros(oldFrame.size(), oldFrame.type());
 
     while (true) {
-        std::vector<cv::Point2f> corners;
+        std::vector<cv::Point2f> corners;  // corners for current frame
         cv::Mat frame;
-        // std::vector<cv::KeyPoint> keypoints;
+        cv::Mat grayMat;  // current greyscale
+
         if (!capture.read(frame)) {
             std::cerr << "Read blank frame\n";
             continue;
         }
 
-        // image processing code here
-
         // greyscale
-        cv::Mat greyMat;
-        cv::cvtColor(frame, greyMat, cv::COLOR_BGR2GRAY);
-        cv::goodFeaturesToTrack(greyMat, corners, maxCorners, qualityLevel,
+
+        cv::cvtColor(frame, grayMat, cv::COLOR_BGR2GRAY);
+        cv::goodFeaturesToTrack(grayMat, corners, maxCorners, qualityLevel,
                                 minDistance, mask, blockSize, useHarrisDetector,
                                 k);
-        for (size_t i = 0; i < corners.size(); i++) {  // draw circles
-            cv::circle(greyMat, corners[i], 3, cv::Scalar(255.),
-                       -1);  // circles on greyscale
-            cv::circle(frame, corners[i], 3, cv::Scalar(255.),
-                       -1);  // cirlces on BGR
-            queue.push(corners[i]);
-            // std::cout << corners[i] << "\n";
+        for (int i = 0; i < corners.size(); ++i) {
+            set(corners[i]);
         }
-        std::cout << queue.front() << "  ";
-        queue.pop();
-        while (!queue.empty()) {
-            std::cout << std::setw(20) << queue.front() << "  ";
-            queue.pop();
-        }
-        std::cout << "\n";
-        // look up distortion/camera calibration
+        // ADD GOODFEATURES (CORNERS) TO SET
+        // PUT SET BACK INTO A VECTOR
 
+        // for (size_t i = 0; i < corners.size(); i++) {  // draw circles
+        // cv::circle(greyMat, corners[i], 3, cv::Scalar(255.),
+        //            -1);  // circles on greyscale
+        // cv::circle(frame, corners[i], 3, cv::Scalar(255.),
+        //            -1);          // cirlces on BGR
+        // queue.push(corners[i]);  // store in queue
+        // }
+        // std::cout << queue.front() << "  ";
+        // queue.pop();
+        // while (!queue.empty()) {  // print queue
+        //     std::cout << std::setw(20) << queue.front() << "  ";
+        //     queue.pop();
+        // }
+        // std::cout << "\n";
+
+        // Calulating the optical flow - finding GOOD POINTS
+        std::vector<uchar> status;
+        std::vector<float> err;
+        cv::TermCriteria criteria = cv::TermCriteria(
+            (cv::TermCriteria::COUNT) + (cv::TermCriteria::EPS), 10, 0.03);
+        // USE THE SET/VECTOR THAT ADDED NEW GOODFEATURES
+        cv::calcOpticalFlowPyrLK(oldGray, grayMat, oldCorners, corners, status,
+                                 err, cv::Size(15, 15), 2, criteria);
+        // cv::calcOpticalFlowPyrLK(oldGray, grayMat, corners, #####newval,
+        // status,
+        //                          err, Size(15, 15), 2, criteria);
+        std::vector<cv::Point2f> good_new;
+        for (uint i = 0; i < oldCorners.size(); i++) {
+            // Select good points
+            if (status[i] == 1) {
+                good_new.push_back(corners[i]);
+                set(corners[i]);
+                // draw the tracks
+                line(mask, corners[i], oldCorners[i], colors[i], 2);
+                circle(frame, corners[i], 5, colors[i], -1);
+            }
+        }
+
+        // look up distortion/camera calibration
         // implement FAST algorithm and find key points
         // cv::drawKeypoints
+        // cv::Mat img;
+        // add(grayMat, mask, img);
         cv::imshow("Tello", frame);
-        cv::imshow("Tello_Grey", greyMat);
+        // cv::imshow("Tello_Grey", grayMat);
+        oldGray = grayMat.clone();
+        oldCorners = good_new;
+        // SAVE SET
         if (cv::waitKey(1) == 27) {  // ESC
             break;
         }
     }
+}
+
+std::set<cv::Point2f> convertToSet(const std::vector<cv::Point2f> v) {
+    // Declaring the set
+    std::set<cv::Point2f> s;
+
+    // Traverse the Vector
+    // for (int i = 0; i < v.size(); i++) {
+    //     // Insert each element
+    //     // into the Set
+    //     s.insert(v[i]);
+    // }
+
+    // for (const auto x : v) {
+    //     // Insert each element
+    //     // into the Set
+    //     s.emplace(x);
+
+    // }
+
+    // std::set<cv::Point2f> s{v.begin(), v.end()};
+
+    // Return the resultant Set
+    return s;
 }
